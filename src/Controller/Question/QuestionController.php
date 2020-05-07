6dda9fa56022bdc6f5a0da9eae1908dc659de67c
Michael <michael@kalitics.com>
2020-05-07 11:28:42 +1000

    Added some Twiggy goodness

---
+++ src/Controller/Question/QuestionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Question;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController {
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('This is my first controller');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig',[
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+        ]);
+    }
+}
